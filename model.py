771df85649ef026146e6582a6d495453f605f934
Kadir Diwan <59998475+diwan-kadir@users.noreply.github.com>
2021-06-16 23:20:41 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,145 @@
+import pickle
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+from sklearn.model_selection import train_test_split
+import collections
+import matplotlib.pyplot as plt
+import pandas as pd
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Activation, Flatten, Input
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers.normalization import BatchNormalization
+
+
+A = open('TCGA_new_pre_second.pckl', 'rb')
+[dropped_genes_final, dropped_gene_name, dropped_Ens_id, samp_id_new, diag_name_new,
+ project_ids_new] = pickle.load(A)
+A.close()
+
+f = open('TCGA_new_pre_first.pckl', 'rb')
+[ensemble_gene_id, gene_name, patient_id, cancer_type, remain_cancer_ids_ind, remain_normal_ids_ind] = pickle.load(f)
+f.close()
+
+## embedding labels
+# integer encode
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(project_ids_new)
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+
+X_cancer_samples =dropped_genes_final.iloc[:,remain_cancer_ids_ind].T.values
+X_normal_samples = dropped_genes_final.iloc[:,remain_normal_ids_ind].T.values
+onehot_encoded_cancer_samples = onehot_encoded[remain_cancer_ids_ind]
+onehot_encoded_normal_samples = onehot_encoded[remain_normal_ids_ind]
+
+X_cancer_samples_mat = np.concatenate((X_cancer_samples,np.zeros((len(X_cancer_samples),9))),axis=1)
+## add nine zeros to the end of each sample
+X_cancer_samples_mat = np.reshape(X_cancer_samples_mat, (-1, 71, 100))
+
+## This line is useful when only one fold training is needed
+x_train, x_test, y_train, y_test = train_test_split(X_cancer_samples_mat, onehot_encoded_cancer_samples,
+                                                    stratify= onehot_encoded_cancer_samples,
+                                                    test_size=0.25, random_state=42)
+
+
+img_rows, img_cols = len(x_test[0]), len(x_test[0][0])
+num_classes = len(y_train[0])
+batch_size = 128
+epochs = 20
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
+input_shape = (img_rows, img_cols, 1)
+
+
+model = Sequential()
+        ## ***** First layer Conv
+model.add(Conv2D(32, kernel_size=(10, 10), strides=(1, 1),
+                         input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(2, 2))
+## *** Classification layer
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['categorical_accuracy'])
+
+
+callbacks = [EarlyStopping(monitor='categorical_accuracy', patience=3, verbose=0)]
+model.summary()
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+history = model.fit(x_train,y_train,batch_size=batch_size,epochs=epochs, callbacks=callbacks,validation_data=(x_test,y_test))
+# model.save('Cancer_model.h5')
+# x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+score = model.evaluate(x_test, y_test, verbose = 0) 
+print('Test loss:', score[0]) 
+print('Test accuracy:', score[1])
+
+
+
+
+
+predict = model.predict_classes(x_test)
+y_test_class = np.where(y_test == 1)[-1]
+y_train_class = np.where(y_train == 1)[-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# p = roc_curve(y_train_class,predict,multi_class='ovo')
+
+
+EPOCHS = 20
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 4))
+# t = f.suptitle('Pre-trained InceptionResNetV2 Transfer Learn with Fine-Tuning & Image Augmentation Performance ', fontsize=12)
+f.subplots_adjust(top=0.85, wspace=0.3)
+
+epoch_list = list(range(1,EPOCHS+1))
+ax1.plot(epoch_list, history.history['categorical_accuracy'], label='Train Accuracy')
+ax1.plot(epoch_list, history.history['val_categorical_accuracy'], label='Validation Accuracy')
+ax1.set_xticks(np.arange(0, EPOCHS+1, 1))
+ax1.set_ylabel('Accuracy Value')
+ax1.set_xlabel('Epoch #')
+ax1.set_title('Accuracy')
+l1 = ax1.legend(loc="best")
+
+ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
+ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
+ax2.set_xticks(np.arange(0, EPOCHS+1, 1))
+ax2.set_ylabel('Loss Value')
+ax2.set_xlabel('Epoch #')
+ax2.set_title('Loss')
+l2 = ax2.legend(loc="best")
